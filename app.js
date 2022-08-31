fc1851001ddb31fed69794013c078843ffd2294b
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2022-08-30 23:26:20 -0300

    Realizando simulador interaactivo en js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-//escoba de 15//
-for (let carta = 1; carta < 15; carta++) {
-    if (carta === 15) {
-        break
-    };
-    console.log(`El valor de carta es $(15)`)
+//simulador primer entrega//
+// function saludar (nombre , apellido) {
+//     alert(`hola ${nombre} ${apellido}` )
+// }
+// saludar ("Ignacio", "Gonzalez")
+// let cajero = prompt ("Desea realizar una operacion")
+// let dinero 
+// if (cajero == "si") {
+//    prompt  ("Ingrese el monto a depositar")
+// } else (dinero == "") 
+// for (let dinero = 4000; dinero < 60000; dinero++) {
+//     if (dinero === "");
+//     break
+// }
+// alert (`El dinero actual es ${64000}`)
+let inicio = prompt ("Ingrese nombre de usuario")
+function saludar (nombre , apellido){
+    alert(`hola ${nombre} ${apellido}`)
 }
-   
+saludar ("Ignacio", "Gonzalez")
+let banca = prompt ("Desea Realizar una operacion")
+let dinero = 5000;
+let saldo = 60000;
+function cajero (dinero, saldo, cajero){
+    switch(cajero){
+        case"+":
+        return dinero + saldo;
+        break
+        case "-":
+        return dinero - saldo;
+        break    
+    }
+}
+alert (cajero (60000,5000, "-"));
