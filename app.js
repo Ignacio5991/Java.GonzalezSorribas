4078f2af334dc84724b53dbc5db19b48a00ca506
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2022-09-07 20:06:37 -0300

    Agregando array al proyecto

--- app.js
+++ app.js
@@ -32,6 +32,7 @@
 //     }
 // }
 // alert (cajero (60000,5000, "-"));
+//Desafio complementario 2
 let identifiquese;
 let ingreseclavedevalidacion;
 let carrito=0;
@@ -65,6 +66,13 @@ function iniciarsesion(){
 }
 function menu(){
     let producto=prompt("Seleccione los perifericos para armar su transformer \n 1-Placa de Video rtx 3090 \n 2-Mother Gigabyte B550M Aorus Pro AX AM4 \n 3-Gabinete Corsair iCUE 5000T RGB Black \n 4-.Volver")
+    const changuito = [];
+let capacidad = 3;
+do {
+    let almacenar = prompt ("Ingrese un periferico")
+    changuito.push (almacenar)
+} while (changuito.length!=capacidad);
+alert("changolleno");
         let precioperiferico=0;
         const preciortx=999999;
         const preciomother=38851;
@@ -102,7 +110,85 @@ function menu(){
                         mostrarMenu();
                         break;
 }}
+
 menu();
 alert("Bienvenido!!!");
 atenderalcomprador();
+
+
+//fin del desafio complementatatio
+
+// let identifiquese;
+// let ingreseclavedevalidacion;
+// let carrito=0;
+// let valorcarrito=0;
+// let cantidaddecomponentescomprados="";
+// let numerorandom=Math.round(Math.random()*2000000);
+// let letrarandom=Math.random().toString(24).replace(/[^a-z]+/g,"");
+// let idaleatorio=(letrarandom.toUpperCase())+numerorandom;
+// function atenderalcomprador(){
+//             let mostrarcatalogo = prompt("Quieres ver el catalogo? Responde si o no");
+//             if (mostrarcatalogo == "si" || mostrarcatalogo=="no"){
+//                 if (mostrarcatalogo == "si"){
+//                 alert("Antes debes iniciar sesion");
+//                 iniciarsesion();
+//                 mostrarmenu();
+//                 }
+//                 else{
+//                 alert("no inicio sesion");
+//                 }
+//             }
+//             else{
+//             alert("Datos incorrectos");
+//             atenderalcomprador();
+//             }
+//         }
+// function iniciarsesion(){
+//     let identifiquese = prompt("Ingrese nombre de usuario");
+//     let ingreseclavedevalidacion = prompt("Ingrese contraseña");
+//     if (identifiquese == "" && ingreseclavedevalidacion == "")
+//     alert ("Ingreso con exito");
+// }
+// function menu(){
+//     let producto=prompt("Seleccione los perifericos para armar su transformer \n 1-Placa de Video rtx 3090 \n 2-Mother Gigabyte B550M Aorus Pro AX AM4 \n 3-Gabinete Corsair iCUE 5000T RGB Black \n 4-.Volver")
+//         let precioperiferico=0;
+//         const preciortx=999999;
+//         const preciomother=38851;
+//         const preciogabinete=85100;
+//             switch(producto){
+//                 case"1":
+//                 function sumaralcarro(){
+//                     if(carrito >=3){
+//                     alert("carro lleno")
+//                     menu()
+//                 }else{
+//                  carrito++
+//                 valorcarrito+precioperiferico;
+//                 cantidaddecomponentescomprados=cantidaddecomponentescomprados+producto
+//                 alert(`Se añadio al carrito ${producto} ${precioperiferico}`)
+//                 return menu()
+//                }
+//             }
+
+//                 producto="Placa de Video rtx 3090,";
+//                 precioperiferico=preciortx;
+//                 sumaralcarro();
+//                 break;
+//                 case "2": 
+//                         producto="Mother Gigabyte B550M Aorus Pro AX AM,";
+//                         precioperiferico=preciomother;
+//                         sumaralcarro();
+//                         break;
+//                 case "3": 
+//                         producto="Gabinete Corsair iCUE 5000T RGB Black";
+//                         precioperiferico=preciogabinete;
+//                         sumaralcarro();
+//                         break;
+//                 case "4": 
+//                         mostrarMenu();
+//                         break;
+// }}
+// menu();
+// alert("Bienvenido!!!");
+// atenderalcomprador();
 
